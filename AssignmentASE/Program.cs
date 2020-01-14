8431bde474bf90bf8ae92739eac39eeccd3a5dbb
Manjit Prasad Hada <manjithada58@gmail.com>
2020-01-15 03:30:19 +0545

    Methods

--- AssignmentASE/Program.cs
+++ AssignmentASE/Program.cs
@@ -12,11 +12,24 @@ namespace AssignmentASE
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+        static void Method1()
+        {
+            Console.WriteLine("Program has just been Executed.");
+            MessageBox.Show("Press Ok to proceed");
+        }
+        static void Method2(string fname)
+        {
+            Console.WriteLine(fname + " Manjit Hada");
+        }
         static void Main()
         {
+            Method2("Program made by");
+            Method1();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
+
         }
     }
 }
